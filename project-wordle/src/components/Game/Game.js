969331feea3013da5c24669aa826abdd575ddd2d
Balázs <balazs@otakomaiya.com>
2024-10-23 22:40:17 +0200

    Joy of react: Project 1, exercises 1,2

---
+++ project-wordle/src/components/Game/Game.js
@@ -0,0 +1,24 @@
+import React from "react";
+
+import { sample } from "../../utils";
+import { WORDS } from "../../data";
+import Input from "../Input";
+import Guesses from "../Guesses";
+
+// Pick a random word on every pageload.
+const answer = sample(WORDS);
+// To make debugging easier, we'll log the solution in the console.
+console.info({ answer });
+
+function Game() {
+  const [guesses, setGuesses] = React.useState([]);
+
+  return (
+    <div>
+      <Guesses guesses={guesses} />
+      <Input onSubmit={(guess) => setGuesses([...guesses, guess])} />
+    </div>
+  );
+}
+
+export default Game;
---
+++ project-wordle/src/components/Input/Input.js
@@ -0,0 +1,35 @@
+import React from "react";
+
+function Input({ onSubmit }) {
+  const [word, setWord] = React.useState("");
+
+  const handleChange = (e) => {
+    setWord(e.target.value.toUpperCase());
+  };
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    console.log(word);
+    onSubmit(word);
+    setWord("");
+  };
+
+  return (
+    <div>
+      <form onSubmit={handleSubmit} className="guess-input-wrapper">
+        <label htmlFor="guess-input">Enter guess</label>
+        <input
+          required={true}
+          value={word}
+          onChange={handleChange}
+          id="guess-input"
+          type="text"
+          minLength={5}
+          maxLength={5}
+        />
+      </form>
+    </div>
+  );
+}
+
+export default Input;
